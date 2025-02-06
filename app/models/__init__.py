d2abe2949bf08d2e003ccfede7e7a7c61e70b733
sarah <zbd0329@gmail.com>
2025-02-06 16:18:53 +0900

    feat: models/__init__.py에 UserMember 모델 등록

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from .base import Base
 from .user_model import User
 from .member_model import Member
+from .users_members import UserMember
 
-__all__ = ['Base', 'User', 'Member']
+__all__ = ['Base', 'User', 'Member', 'UserMember']
